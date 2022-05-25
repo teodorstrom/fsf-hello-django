4f84349d33128dd47afe5a494cc99be1f45a16a5
teodorstrom <teodorstrom.ts@gmail.com>
2022-05-25 10:05:05 +0000

    post and get data between page and database correctly

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
+"""
+models for database
+"""
+
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    """
+    set rules for name and done items
+    """
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
